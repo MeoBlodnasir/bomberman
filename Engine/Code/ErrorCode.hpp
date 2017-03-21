12e0853bd073dc0ed69abec199a1c18ef0ad6b01
djo <diccy91@hotmail.com>
2017-03-21 21:39:17 +0100

    StrongPointer

---
+++ Engine/Code/ErrorCode.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace ft
+{
+	enum EErrorCode
+	{
+		FT_OK = 0,
+		FT_FAIL = 0x00010000
+	};
+
+	// Faire une classe ErrorCode qui vérifie qu'on la vérifie
+	typedef EErrorCode	ErrorCode; // tmp
+}
