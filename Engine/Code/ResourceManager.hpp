6b2bfd04e3a953953111556ac52ffb696456c02b
djo <diccy91@hotmail.com>
2017-03-28 04:10:08 +0200

    Interface de ResourceManager

---
+++ Engine/Code/ResourceManager.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "ErrorCode.hpp"
+#include "StrongPointer.hpp"
+#include "Hash.hpp"
+
+#include <map>
+#include <type_traits>
+
+namespace ft
+{
+	// L'Id de la Resource est une classe qui permet de récupérer une référence
+	// sur cette-ci via un ResourceManager correspondant.
+	// En général std::string, ft:Path, ou int.
+	template <typename TId>
+	struct Resource : public CountableSPtr
+	{
+		typedef	TId		Id;
+	};
+
+	template <typename TResourceType>
+	class ResourceManager
+	{
+	public:
+
+		// Vérification à la compilation que le type passé en argument de template est une Resource
+		FT_STATIC_ASSERT((std::is_base_of< Resource<typename TResourceType::Id>, typename TResourceType >::value));
+		
+		typedef	typename TResourceType::Id	Id;
+		typedef	typename TResourceType		Type;
+
+		// Charge et alloue la ressource si elle ne l'est pas encore puis garde une correspondance dans la map,
+		// sinon récupère la ressource dans la map,
+		// renvoie la ressource par le StrongPointer donné en paramètre.
+		// FT_OK si tout s'est bien passé,
+		// FT_FAIL si la ressource n'a pas pu être chargée
+		virtual ErrorCode	Load(const Id&, SPtr<Type>&) = 0;
+
+		// Supprime la référence sur la ressource correspondante.
+		// FT_OK si la ressource est désallouée,
+		// FT_FAIL si une référence existe encore sur cette ressource, hors ResourceManager
+		virtual ErrorCode	Unload(const Id&) = 0;
+
+		// Supprime les références sur toutes les ressources.
+		// FT_OK si tout est désalloué.
+		// FT_FAIL si au moins une référence existe encore sur une ressource, hors ResourceManager
+		virtual ErrorCode	UnloadAll() = 0;
+
+	protected:
+
+		std::map<Hash, SPtr<Type>>		m_oResources;
+	};
+}
